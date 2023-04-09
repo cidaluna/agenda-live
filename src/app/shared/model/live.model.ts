30e353b22b85637a70de1f440d20db055626673c
cidaluna <cidalunna@hotmail.com>
2023-04-09 18:44:54 -0300

    Created models

---
+++ src/app/shared/model/live.model.ts
@@ -0,0 +1,10 @@
+export class Live{
+    // Aula 07 Criando Live Model, realizando o mapeamento dos atributos que o Backend retorna para o Front.
+    id: string;
+    liveName: string;
+    channelName: String;
+    liveDate: string;
+    liveTime: string;
+    liveLink: string;
+    registrationDate: string;
+}
---
+++ src/app/shared/model/responsePageable.model.ts
@@ -0,0 +1,13 @@
+export class ResponsePageable{
+    // Aula 07 Criando pageable model, realizando o mapeamento dos atributos que o Backend retorna para o Front.
+    content: any[];
+    first: boolean;
+    last: boolean;
+    number: number;
+    numberOfElements: number;
+    pageable: any[];
+    size: number;
+    sort: number;
+    totalElements: number;
+    totalPages: number;
+}
