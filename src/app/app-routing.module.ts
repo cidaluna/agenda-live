9e91c7aa84645440bbb175aedaa42e441026d538
cidaluna <cidalunna@hotmail.com>
2023-04-09 09:26:20 -0300

    Created home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', 
+    component: HomeComponent   //Aula 03: definindo a página principal chamando o componente Home
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
